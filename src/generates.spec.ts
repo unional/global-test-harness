82ac76a16dcb6f04049cd91183d9c04c0f2f3ce6
Homa Wong <homawong@gmail.com>
2019-06-05 01:38:09 -0700

    chore: convert to jest (#52)

--- src/generates.spec.ts
+++ src/generates.spec.ts
@@ -1,68 +1,67 @@
-import test from 'ava';
 import { generateNamespaceToRelativeFunction, generateRelativeToNamespaceFunction } from './generates';
 
-test('gets base namespace', t => {
+test('gets base namespace', () => {
   const ns = {
     'My': './fixtures/my'
   }
 
   const fn = generateNamespaceToRelativeFunction('.', ns)
 
-  t.is(fn('My'), './fixtures/my')
+  expect(fn('My')).toBe('./fixtures/my')
 })
 
-test('gets longer namespace over shorter one', t => {
+test('gets longer namespace over shorter one', () => {
   const ns = {
     'My': './fixtures/my',
     'My.sub': './another-place'
   }
 
   const fn = generateNamespaceToRelativeFunction('.', ns)
-  t.is(fn('My.sub'), './another-place')
+  expect(fn('My.sub')).toBe('./another-place')
 })
 
-test('convert sub-namespaces to path', t => {
+test('convert sub-namespaces to path', () => {
   const ns = {
     'My': './fixtures/my'
   }
 
   const fn = generateNamespaceToRelativeFunction('.', ns)
 
-  t.is(fn('My.x.y'), './fixtures/my/x/y')
+  expect(fn('My.x.y')).toBe('./fixtures/my/x/y')
 })
 
-test('map with different root', t => {
+test('map with different root', () => {
   const ns = {
     'My': './my'
   }
 
   const fn = generateNamespaceToRelativeFunction('./fixtures', ns)
 
-  t.is(fn('My'), './my')
+  expect(fn('My')).toBe('./my')
 })
 
-test('not in namespace list returns a strict translated namespace', t => {
+test('not in namespace list returns a strict translated namespace', () => {
   const ns = { My: './my' }
   const fn = generateRelativeToNamespaceFunction('./fixtures/top', ns)
-  t.is(fn('./foo'), 'foo')
+  expect(fn('./foo')).toBe('foo')
 })
 
 
-test('matching namespace returns it', t => {
+test('matching namespace returns it', () => {
   const ns = { My: './my' }
   const fn = generateRelativeToNamespaceFunction('./fixtures/top', ns)
-  t.is(fn('./my'), 'My')
+  expect(fn('./my')).toBe('My')
 })
 
-test('sub-path maps as sub-namespace', t => {
+test('sub-path maps as sub-namespace', () => {
   const ns = { My: './my' }
   const fn = generateRelativeToNamespaceFunction('./fixtures/top', ns)
-  t.is(fn('./my/component'), 'My.component')
+  expect(fn('./my/component')).toBe('My.component')
 })
 
-test('match longer paths before shorter one', t => {
+test('match longer paths before shorter one', () => {
   const ns = { 'My.bigComponent': './my', 'A.b': './my/comp' }
   const fn = generateRelativeToNamespaceFunction('./fixtures/top', ns)
-  t.is(fn('./my/y'), 'My.bigComponent.y')
-  t.is(fn('./my/comp'), 'A.b')
+  expect(fn('./my/y')).toBe('My.bigComponent.y')
+  expect(fn('./my/comp')).toBe('A.b')
 })
